0568081a48c4db939e8c58677ef7ff3d9d2ae896
Yvan Cluet <ycluet@bs406-s16-15.insa-lyon.fr>
2017-01-23 17:13:43 +0100

    modifications qui marchent pas (étudiant 1)

--- string.cpp
+++ string.cpp
@@ -2,9 +2,121 @@
 #include <cstdio>
 #include <cstdlib>
 
-String::String(){
-    char tab[20];
-    int size = 0;
-    int sizemax = 0;
-    tab[size]='\0';
+const int String::Sizemax = 100;
+
+////////////////////// CONSTRUCTORS
+
+String::String(){ //default constructor
+    tab_ = nullptr;
+    size_= 0;
+    capacity_ = 0;
 }
+
+String::String(int n, char c){
+    size_=n;
+    capacity_=getCap(n);
+    tab_=new char[capacity_+1];
+    for (int i =0; i<n; i++){
+      tab_[i]=c;
+    }
+    tab_[n]='\0';
+}
+
+String::String (const String &s){ //copy constructor
+    size_=s.size();
+    capacity_=getCap(size_);
+    tab_= new char[capacity_+1];
+    char* ptr = s.c_str();
+    for(int i=0; i<size_;i++)
+    tab_[i]=ptr[i];
+    tab_[size_]='\0';
+}
+
+///////////////////////// PROTECTED METHODS
+
+//added method providing a tab capacity for a given size
+int String::getCap(int size){
+    size=size*2;
+    if (size>(Sizemax/2))
+	return Sizemax;
+    else
+	return size;
+}
+
+
+////////////////////////// PUBLIC METHODS
+
+
+const char* String::c_str(){
+    return tab_;
+}
+
+
+void String::clear() {
+    tab_[0]='\0';
+    size_=0;
+}
+
+
+////////////////////////////// GETTERS
+
+int String::max_size(){
+    return Sizemax;
+}
+
+int String::size() {
+    return size_;
+}
+
+int String::capacity() {
+    return capacity_;
+}
+
+char* String::tab() {
+    return tab_;
+}
+
+////////////////////////////// OPERATORS
+
+String& String::operator= (const String &S){
+	
+  capacity_ = S.capacity();
+	size_ = S.size();
+	delete [] tab_;
+	tab_ = new char[capacity_+1];
+	
+	char* cA = S.c_str();
+	for (int i= 0 ; i<size_+1 ; i++){
+		tab_[i] = cS[i];
+	}
+	
+	return *this;
+}
+
+
+
+String operator+ (const String &S, char C){
+	
+  char* tab_S = S.c_str();
+  
+  int x = S.size();
+  int sumsize = x + 1;
+	
+  if (sumsize > S.Sizemax){
+    sumsize = S.Sizemax;
+    std::cout << "length of string exceeds maximum size"  << std::endl;
+  }
+  
+  char* sumtab = new char[sumsize+1];
+	
+  for (int i =0; i<x; i++){
+    sumtab[i]=tab_S[i];
+  }
+	
+  sumtab[x]=C;
+  sumtab[sumsize] = '\0';
+  
+  return String(sumtab);
+}
+
+
--- string.h
+++ string.h
@@ -2,49 +2,63 @@
 #include <cstdlib>
 
 class String {
- protected:
+ private:
+ 
+    //////////////// ATTRIBUTES
+    
     // length of the String
     int size_;
     // tab of char (represents the string)
     char* tab_;
     // tab size (room currently allocated)
-    int sizemax_;
+    int capacity_;
+    //maximum possible tab room
+    static const int Sizemax;
     
-    size_t getCapacity(size_t size); 
-    static int length(const char* s);
     
+    
+    ////////////////// PRIVATE METHODS
+    
+    int getCap(int size); 
+    
+    
+    
+        
  public:
     
-    //Constructors
+    ////////////////// CONSTRUCTORS
+    
     String(); //default constructor
-    String(const String& str); //constructor by copy
-    String(char* chartab); //constructor 
-
-    //Destructor
-    ~String();
-   
-    // Public Methods
+    String(const String& s); //constructor by copy
+    String(int n, char c);
+    
+    
+    
+    ////////////////// PUBLIC METHODS
+    
     char* c_str();
     void clear ();
-    bool empty();
-    int size(); //length() will not be added because it's identical to size
-    int max_size();
-    void resize (int n);
-    int capacity();
-    void reserve(int n);
 
- 
-    //Operators
-    String& operator=(const char* other);
-    String& operator=(const String elem);
-    String& operator=(char c);
-    // operator + overloading  with string given
-    char& operator[] (int pos);
-    const char& operator[] (int pos) const;
+
+
+    ////////////////// OPERATORS
+    
+    String& operator= (const String& S);
+    friend String operator+ (const String& S, char C);
+    
+     
+    
+    //////////////////// GETTERS
+    
+    inline int max_size();
+    inline int size();
+    inline int capacity();
+    inline char* tab();     
+    //////////////////// SETTERS
+    
+    
+    
+    
 };
-String operator+(const String& s, const char c);
-String operator+(const char c, const String& s);
-String operator+(const String& lhs, const String& rhs);
-String operator+(const String& lhs, const char* rhs);
-String operator+(const char* lhs, const String& rhs);
+
 
